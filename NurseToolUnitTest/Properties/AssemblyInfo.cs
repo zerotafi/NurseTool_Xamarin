5713729dfebd8e58c159c05ff196afa6ce5b990a
zoltan tafferner <tafimic@gmail.com>
2018-03-04 18:53:14 +0100

    UnitTestProject

---
+++ NurseToolUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NurseToolUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NurseToolUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("797a90ee-aa62-46b0-bf8a-21ac68a0fead")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ NurseTool_Xamarin/NurseTool_Xamarin/Helpers/HL7_manager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NHapi;
+
+namespace NurseTool_Xamarin.Helpers
+{
+    class HL7_manager
+    {
+    }
+}
